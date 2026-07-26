a07d9754d8b7136d8c7f12075f205db3b4baf3bb
Studentcuchd <parag280806@gmail.com>
2026-07-26 14:14:00 +0530

    Project Updated

---
+++ @PROJECT IMPLEMENTATION/App/app_functions.py
@@ -0,0 +1,197 @@
+from Model.internclass import Intern
+from Model.mentorclass import Mentor
+from Model.problemclass import Problem
+
+from Repository.intern_repository import InternRepository
+from Repository.problem_repository import ProblemRepository
+from Repository.mentor_repository import MentorRepository
+
+from Service.mentor_matching_service import MentorMatchingService
+
+
+def print_menu() -> None:
+    print("\n----------  Intern Mentor Matching System  ----------")
+    print("1. Add Intern")
+    print("2. Add Mentor")
+    print("3. Add Problem")
+    print("4. View Interns")
+    print("5. View Mentors")
+    print("6. View Problems")
+    print("7. Find Matching Mentors")
+    print("8. Exit")
+ 
+ 
+    
+def add_intern(intern_repository:InternRepository) -> None:
+    name=input("enter your name=").strip()
+    email=input("enter you eamil=").strip()
+    intern_obj=Intern(name,email)
+    intern_repository.add_intern(intern_obj)
+    print("Intern Added successfully")
+
+    
+
+def view_interns(intern_repository:InternRepository) ->None:
+    interns_list=intern_repository.get_all_interns()
+     
+    if not interns_list:
+        print("No intern found Please add Intern")
+        return
+    
+    print("\n------ Interns ------")
+    
+    for intern in interns_list:
+        print(f"ID= {intern.intern_id}")
+        print(f"Name= {intern.name}")
+        print(f"Email= {intern.email}")
+        
+        print("**********************************************\n")
+ 
+ 
+        
+
+def add_mentor(mentor_repository:MentorRepository) -> None:
+    name=input("enter your name=").strip()
+    email=input("enter you eamil=").strip()
+    
+    skill_set=set()
+    total_skills=int(input("Enter total number of skills you have="))
+    print("......Enter your skills one by one......")
+    for i in range(total_skills):
+        skill_input=input(f"Enter your {i+1} skill=").strip()
+        if skill_input:
+            skill_set.add(skill_input)
+
+    mentor_obj=Mentor(name,email,skill_set)
+    mentor_repository.add_mentor(mentor_obj)
+    
+    print("Mentor added successfully.")   
+
+
+
+
+def view_mentors(mentor_repository:MentorRepository) -> None:
+    mentor_list=mentor_repository.get_all_mentors()
+    
+    if not mentor_list:
+        print("No Mentor found Please add a mentor")
+        return 
+    
+    print("\n------ Mentors ------")
+       
+    for mentor in mentor_list:
+        print(f"ID= {mentor.mentor_id}")
+        print(f"Name= {mentor.name}")
+        print(f"Email= {mentor.email}")
+        print(f"Skills= {','.join(mentor.expertise)}")
+        print("**********************************************\n")
+        
+
+
+        
+def add_problem(problem_repository: ProblemRepository,intern_repository:InternRepository) -> None:
+    interns_list= intern_repository.get_all_interns()
+    if not interns_list:
+        print("No intern found Please add Intern")
+        return
+    
+    view_interns(intern_repository)
+    
+    intern_id=int(input("Enter Your Intern ID="))
+    
+    valid_intern=False
+    
+    for intern in interns_list:
+        if intern.intern_id==intern_id:
+            valid_intern=True
+            break
+    
+    if not valid_intern:
+        print("Invalid Intern id")
+        return
+    
+    title = input("Enter Problem Title= ").strip()
+    description = input("Enter Problem Description= ").strip()
+    
+    required_skills=set()
+    
+    total_skills = int(input("Enter total required skills: "))
+    
+    print("\n")
+    print("Enter required skills one by one")
+    print("\n")
+    
+    for i in range(total_skills):
+        skill=input(f"Enter {i+1} skill=").strip()
+
+        if skill:
+            required_skills.add(skill)
+    
+    
+    problem_obj = Problem(
+        title,
+        description,
+        required_skills,
+        intern_id
+    )
+
+    problem_repository.add_problem(problem_obj)
+
+    print("Problem added successfully.")
+
+
+
+
+
+def view_problems(problem_repository: ProblemRepository) -> None:
+
+    problem_list = problem_repository.get_all_problems()
+
+    if not problem_list:
+        print("No problems found.")
+        return
+
+    print("\n------ Problems ------")
+
+    for problem in problem_list:
+        print(f"Problem ID = {problem.problem_id}")
+        print(f"Title= {problem.title}")
+        print(f"Description= {problem.description}")
+        print(f"Intern ID= {problem.intern_id}")
+        print(f"Required Skills= {', '.join(problem.req_skill)}")
+        print("**********************************************\n")
+  
+  
+        
+        
+
+def find_matching_mentors(problem_repository: ProblemRepository,matching_service: MentorMatchingService) -> None:
+    problem_list = problem_repository.get_all_problems()
+
+    if not problem_list:
+        print("No Problem posted right now please add problem")
+        return   
+    
+    view_problems(problem_repository)
+    problem_id=int(input("Pleease enter id of your problem="))
+    
+    matching_mentors_list=matching_service.find_matching_mentors(problem_id)
+    
+    if not matching_mentors_list:
+        print("No match found")
+        return
+    
+    print("\n***Matching Mentors***")
+    
+    for match in matching_mentors_list:
+        
+        mentor=match['mentor']
+        print(f"Mentor id= {mentor.mentor_id}")
+        print(f"Mentor name= {mentor.name}")
+        print(f"Mentor email= {mentor.email}")
+        print(f"Total Number of matched skills= {match['score']} / {match['total_required_skills']}")
+        print(f"Matched skills= {",".join(match['matched_skills'])}")
+        
+        print("\n*********************************************\n")
+        
+
--- @PROJECT IMPLEMENTATION/main.py
+++ @PROJECT IMPLEMENTATION/main.py
@@ -1,203 +1,52 @@
-from Repository.database_manager import DataBaseManager
+from Repository.database import Database
+
+from Repository.skill_repository import SkillRepository
+from Repository.intern_repository import InternRepository
+from Repository.mentor_repository import MentorRepository
+from Repository.problem_repository import ProblemRepository
+
 from Service.mentor_matching_service import MentorMatchingService
 
-from Model.internclass import Intern
-from Model.mentorclass import Mentor
-from Model.problemclass import Problem
-
-
-def print_menu() -> None:
-    print("\n----------  Intern Mentor Matching System  ----------")
-    print("1. Add Intern")
-    print("2. Add Mentor")
-    print("3. Add Problem")
-    print("4. View Interns")
-    print("5. View Mentors")
-    print("6. View Problems")
-    print("7. Find Matching Mentors")
-    print("8. Exit")
-
-
-def add_intern(database: DataBaseManager) -> None:
-    name=input("enter your name=")
-    email=input("enter you eamil=")
-    intern_obj=Intern(name,email)
-    database.add_intern(intern_obj)
-    print("Intern Added successfully")
-
-
-
-def view_interns(database: DataBaseManager) ->None:
-    interns_list=database.get_all_interns()
-     
-    if not interns_list:
-        print("No intern found Please add Intern")
-        return
-    
-    print("\n------ Interns ------")
-    
-    for intern in interns_list:
-        print(f"ID= {intern.intern_id}")
-        print(f"Name= {intern.name}")
-        print(f"Email= {intern.email}")
-        
-        print("**********************************************\n")
-        
-
-        
-        
-        
-
-def add_mentor(database: DataBaseManager) -> None:
-    name=input("enter your name=")
-    email=input("enter you eamil=")
-    
-    skill_set=set()
-    total_skills=int(input("Enter total number of skills you have="))
-    print("......Enter your skills one by one......")
-    for i in range(total_skills):
-        skill_input=input(f"Enter your {i+1} skill=").strip()
-        if skill_input:
-            skill_set.add(skill_input)
-
-    mentor_obj=Mentor(name,email,skill_set)
-    database.add_mentor(mentor_obj)
-    
-    print("Mentor added successfully.")   
-        
-
-
-def view_mentors(database: DataBaseManager) -> None:
-    mentor_list=database.get_all_mentors()
-    
-    if not mentor_list:
-        print("No Mentor found Please add a mentor")
-        return 
-    
-    print("\n------ Mentors ------")
-       
-    for mentor in mentor_list:
-        print(f"ID= {mentor.mentor_id}")
-        print(f"Name= {mentor.name}")
-        print(f"Email= {mentor.email}")
-        print(f"Skills= {','.join(mentor.expertise)}")
-        print("**********************************************\n")
-
-    
-    
-    
-        
-
-           
-
-def add_problem(database: DataBaseManager) -> None:
-    interns_list=database.get_all_interns()
-    if not interns_list:
-        print("No intern found Please add Intern")
-        return
-    
-    view_interns(database)
-    
-    intern_id=int(input("Enter Your Intern ID="))
-    
-    valid_intern=False
-    
-    for intern in interns_list:
-        if intern.intern_id==intern_id:
-            valid_intern=True
-            break
-    
-    if not valid_intern:
-        print("Invalid Intern id")
-        return
-    
-    title = input("Enter Problem Title= ").strip()
-    description = input("Enter Problem Description= ").strip()
-    
-    required_skills=set()
-    
-    total_skills = int(input("Enter total required skills: "))
-    
-    print("\n")
-    print("Enter required skills one by one")
-    print("\n")
-    
-    for i in range(total_skills):
-        skill=input(f"Enter {i+1} skill=").strip()
-
-        if skill:
-            required_skills.add(skill)
-    
-    
-    problem_obj = Problem(
-        title,
-        description,
-        required_skills,
-        intern_id
-    )
-
-    database.add_problem(problem_obj)
-
-    print("Problem added successfully.")
+from App.app_functions import (
+    print_menu,
+    add_intern,
+    view_interns,
+    add_mentor,
+    view_mentors,
+    add_problem,
+    view_problems,
+    find_matching_mentors,
+)
 
 
 
 
-def view_problems(database: DataBaseManager) -> None:
-
-    problem_list = database.get_all_problems()
-
-    if not problem_list:
-        print("No problems found.")
-        return
-
-    print("\n------ Problems ------")
-
-    for problem in problem_list:
-        print(f"Problem ID = {problem.problem_id}")
-        print(f"Title= {problem.title}")
-        print(f"Description= {problem.description}")
-        print(f"Intern ID= {problem.intern_id}")
-        print(f"Required Skills= {', '.join(problem.req_skill)}")
-        print("**********************************************\n")
-
-
-def find_matching_mentors(database :DataBaseManager, matching_service: MentorMatchingService) -> None:
-    problem_list = database.get_all_problems()
-
-    if not problem_list:
-        print("No Problem posted right now please add problem")
-        return   
-    
-    view_problems(database)
-    problem_id=int(input("Pleease enter id of your problem="))
-    
-    matching_mentors_list=matching_service.find_matching_mentors(problem_id)
-    
-    if not matching_mentors_list:
-        print("No match found")
-        return
-    
-    print("\n***Matching Mentors***")
-    
-    for match in matching_mentors_list:
-        
-        mentor=match['mentor']
-        print(f"Mentor id= {mentor.mentor_id}")
-        print(f"Mentor name= {mentor.name}")
-        print(f"Mentor email= {mentor.email}")
-        print(f"Total Number of matched skills= {match['score']} / {match['total_required_skills']}")
-        print(f"Matched skills= {",".join(match['matched_skills'])}")
-        
-        print("\n*********************************************\n")
-        
 
 
 def main() -> None:
+    database = Database("MatchingSystem.db")
 
-    database = DataBaseManager("DataofClass.db")
+    skill_repository = SkillRepository(database)
 
-    matching_service = MentorMatchingService(database)
+    intern_repository = InternRepository(database)
+
+
+    mentor_repository = MentorRepository(
+        database,
+        skill_repository
+    )
+
+    problem_repository = ProblemRepository(
+        database,
+        skill_repository
+    )
+
+    matching_service = MentorMatchingService(
+        mentor_repository,
+        problem_repository
+    )
+
+
 
     while True:
 
@@ -207,25 +56,25 @@ def main() -> None:
             choice = int(input("\nEnter your choice: "))
 
             if choice == 1:
-                add_intern(database)
+                add_intern(intern_repository)
 
             elif choice == 2:
-                add_mentor(database)
+                add_mentor(mentor_repository)
 
             elif choice == 3:
-                add_problem(database)
+                add_problem(problem_repository,intern_repository)
 
             elif choice == 4:
-                view_interns(database)
+                view_interns(intern_repository)
 
             elif choice == 5:
-                view_mentors(database)
+                view_mentors(mentor_repository)
 
             elif choice == 6:
-                view_problems(database)
+                view_problems(problem_repository)
 
             elif choice == 7:
-                find_matching_mentors(database,matching_service)
+                find_matching_mentors(problem_repository,matching_service)
 
             elif choice == 8:
                 database.close()
